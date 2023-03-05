002ec2772e730811a64e9b386cbd5236520063d0
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2023-03-05 14:24:56 +0100

    Additions to the setup file and testportal
    The files together supply the last task of testing our JDBC code in terms of different outcomes of our database regarding registering and unregistering.
    
    One thing that still needs to be taken care of is the test for sql injection.

--- src/TestPortal.java
+++ src/TestPortal.java
@@ -12,35 +12,84 @@ public class TestPortal {
          // Write your tests here. Add/remove calls to pause() as desired. 
          // Use println instead of prettyPrint to get more compact output (if your raw JSON is already readable)
 
+          //List info for a student.
 
-          System.out.println("register student:"); // SUPPOSED TO FAIL
+          System.out.println((c.getInfo("5555555555")));
+          pause();
+
+          //Register a student for an unrestricted course, and check that he/she ends up registered (print info again).
+          // Should pass
+          System.out.println(c.register("7777777777", "CCC555"));
+          System.out.println((c.getInfo("7777777777")));
+          pause();
+
+          //Register the same student for the same course again, and check that you get an error response.
+
+          // Should fail
+          System.out.println(c.register("7777777777", "CCC555"));
+          pause();
+
+          //Unregister the student from the course, and then unregister him/her again from the same course.
+          //Check that the student is no longer registered and that the second unregistration gives an error response.
+
+          System.out.println(c.register("7777777777", "CCC555"));
+          System.out.println((c.getInfo("7777777777")));
+          //should pass
+          pause();
+
+          System.out.println(c.register("7777777777", "CCC555"));
+          // Should fail.
+          pause();
+
+
+          //Register the student for a course that he/she does not have the prerequisites for, and check that an error is generated.
+
           System.out.println(c.register("7777777777", "CCC123"));
-          prettyPrint(c.getInfo("7777777777"));
-          pause();
-
-          System.out.println("Unregister student:"); // SUPPOSED TO PASS
-          System.out.println(c.unregister("2222222222", "CCC333"));
-          prettyPrint(c.getInfo("2222222222"));
+          //Should fail.
           pause();
 
 
-          System.out.println("register student:"); // SUPPOSED TO PASS
-          System.out.println(c.register("2222222222", "CCC333"));
-          prettyPrint(c.getInfo("2222222222"));
+          //Unregister a student from a restricted course that he/she is registered to, and which has at least two students in the queue.
+          System.out.println(c.getInfo("5555555555"));
+          System.out.println(c.unregister("5555555555", "CCC999"));
           pause();
 
-          System.out.println("Unregister student:"); // SUPPOSED TO Pass
-          System.out.println(c.unregister("7777777777", "CCC444"));
-          prettyPrint(c.getInfo("7777777777"));
-          pause();
+          //Register the student again to the same course and check that the student gets the correct (last) position in the waiting list.
 
-          System.out.println("Unregister student:"); // SUPPOSED TO FAIL
-          System.out.println(c.unregister("7777777777", "CCC444"));
-          prettyPrint(c.getInfo("7777777777"));
+          System.out.println(c.getInfo("5555555555"));
+          System.out.println(c.register("5555555555", "CCC999"));
+          System.out.println(c.getInfo("5555555555"));
           pause();
 
 
-      
+          //Unregister and re-register the same student for the same restricted course, and check that the student is first removed and then ends up in the same position as before (last).
+          System.out.println(c.getInfo("6666666666"));
+          System.out.println(c.unregister("6666666666", "CCC000"));
+          System.out.println(c.getInfo("6666666666"));
+          pause();
+
+          System.out.println(c.register("6666666666", "CCC000"));
+          System.out.println(c.getInfo("6666666666"));
+          pause();
+
+          //Unregister a student from an overfull course, i.e. one with more students registered than there are places on the course (you need to set this situation up in the database directly).
+          //Check that no student was moved from the queue to being registered as a result.
+
+          System.out.println(c.getInfo("4444444444"));
+          System.out.println(c.getInfo("5555555555"));
+          System.out.println(c.getInfo("6666666666"));
+
+          System.out.println(c.unregister("4444444444", "CCC456"));
+          System.out.println(c.getInfo("6666666666"));
+          pause();
+
+          //Unregister with the SQL injection you introduced, causing all (or almost all?) registrations to disappear.
+          //TODO: CREATE THE SQL INJECTION THAT SUPPLIES THE ABOVE REQUIREMENT
+
+
+
+
+          
       } catch (ClassNotFoundException e) {
          System.err.println("ERROR!\nYou do not have the Postgres JDBC driver (e.g. postgresql-42.5.1.jar) in your runtime classpath!");
       } catch (Exception e) {
